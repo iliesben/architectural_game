3230d66139bac90fd91ff1aa1b2bf55dfeca0c53
FRANCOIS Julien <julien.francois@hetic.net>
2022-09-06 11:52:03 +0200

    add all test for elements

--- front/src/utils/game.spec.ts
+++ front/src/utils/game.spec.ts
@@ -23,9 +23,32 @@ test("Test function game Turn with undefined element", () => {
   expect(gameTest).toEqual(undefined)
 });
 
+test("Test function game Turn with same element", () => {
+  const sameElementGrass = gameTurn(grass, grass)
+  const sameElementWater = gameTurn(water, water)
+  const sameElementFire = gameTurn(fire, fire)
+  expect(sameElementGrass).toEqual('Same')
+  expect(sameElementWater).toEqual('Same')
+  expect(sameElementFire).toEqual('Same')
+});
+
 test("Test function game Turn with grass lose or win", () => {
   const loseGrass = gameTurn(grass, fire)
   const winGrass = gameTurn(fire, grass)
   expect(loseGrass).toEqual('Lose')
   expect(winGrass).toEqual('Win')
+});
+
+test("Test function game Turn with fire lose or win", () => {
+  const loseFire = gameTurn(fire, water)
+  const winFire = gameTurn(fire, grass)
+  expect(loseFire).toEqual('Lose')
+  expect(winFire).toEqual('Win')
+});
+
+test("Test function game Turn with water lose or win", () => {
+  const loseWater = gameTurn(water, grass)
+  const winWater = gameTurn(water, fire)
+  expect(loseWater).toEqual('Lose')
+  expect(winWater).toEqual('Win')
 });
