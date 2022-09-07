a61426b93fc05e17ed98da38be831f8ce1adbc2d
Ilies <benosman.ilies@hetic.net>
2022-09-07 17:23:26 +0200

    game interface

--- front/src/pages/Game.tsx
+++ front/src/pages/Game.tsx
@@ -1,8 +1,23 @@
 // recupere le socket si on rejoint, sinon créer un
 import React from "react";
+import styled from "styled-components";
+import { Arena } from '../components/Arena'
+import { Avatar } from "../components/Avatar";
 
 export const Game = () => {
     return (
-        <h1>Game page</h1>
+      <GameContainer>
+        <Arena>
+          <Avatar />
+        </Arena>
+      </GameContainer>
     );
 }
+
+
+const GameContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-top: 2rem;
+`;
