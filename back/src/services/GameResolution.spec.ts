f69cb79e6c7efe060577f91fa3ec1612c0556f31
PiRB <pierrebrivio14@gmail.com>
2022-09-09 16:22:46 +0200

    add tests

---
+++ back/src/services/GameResolution.spec.ts
@@ -0,0 +1,34 @@
+import {describe, expect, test} from '@jest/globals';
+import IPlayer from '../classes/IPlayer';
+import { GameResolution } from './gameResolution';
+
+describe('Test playTurn() function', () => {
+
+  test('first player should win', () => {
+    let player1: IPlayer = new IPlayer('::1', 'player 1', '1')
+    let player2: IPlayer = new IPlayer('::1', 'player 2', '2')
+
+    player1.setCurrentUserChoice('fire')
+    player2.setCurrentUserChoice('grass')
+
+    const players = [player1, player2]
+
+    GameResolution.playTurn(players)
+    expect(player1.nbWin).toEqual(1)
+    expect(player2.nbWin).toEqual(0)
+  })
+
+  test('second player should win', () => {
+    let player1: IPlayer = new IPlayer('::1', 'player 1', '1')
+    let player2: IPlayer = new IPlayer('::1', 'player 2', '2')
+
+    player1.setCurrentUserChoice('fire')
+    player2.setCurrentUserChoice('water')
+
+    const players = [player1, player2]
+
+    GameResolution.playTurn(players)
+    expect(player1.nbWin).toEqual(0)
+    expect(player2.nbWin).toEqual(1)
+  })
+})
