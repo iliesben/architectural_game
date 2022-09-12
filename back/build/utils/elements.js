dad130a4e81cf2efb7723ea137dee1906a3523e3
Haris Souici <souici.haris@gmail.com>
2022-09-12 02:53:30 +0200

    FEATURE: Production update

--- back/build/utils/elements.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.ElementId = void 0;
-exports.ElementId = new Map([
-    ['fire', 1],
-    ['water', 2],
-    ['grass', 3],
-    ['iron', 4],
-]);
