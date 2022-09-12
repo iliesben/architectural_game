b7679e6813cb3c776dcdb6ac8eb1c11e59879df1
iliesben <ilies.benosman@hetic.net>
2022-09-12 02:30:37 +0200

    add env

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -3,16 +3,8 @@ import { ThemeProvider } from "styled-components";
 import { Router } from "./Router";
 import { theme } from "./theme/theme";
 import { SocketContext, socket } from "./context/socket";
-// import { disconnectSocket, initiateSocketConnection } from "./services/socketio.service";
 
 function App() {
-  // useEffect(() => {
-  //   initiateSocketConnection();
-  //   return () => {
-  //     disconnectSocket();
-  //   }
-  // }, []);
-
     return (
       <ThemeProvider theme={theme}>
         <SocketContext.Provider value={socket}>
--- front/src/context/socket.ts
+++ front/src/context/socket.ts
@@ -1,5 +1,5 @@
 import { createContext } from "react";
 import { io, Socket } from "socket.io-client";
 
-export const socket = io("http://localhost:3000")
+export const socket = io(import.meta.env.VITE_API_URL)
 export const SocketContext = createContext<null | Socket>(null);
